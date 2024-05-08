28a70a69f3442a22665e6cc3f92466543db3ecd0
Sachinsingh1608 <sk379403@gmail.com>
2024-05-08 14:44:10 +0530

    main

--- MyConsoleApp/SnakeAndLadder.cs
+++ MyConsoleApp/SnakeAndLadder.cs
@@ -8,48 +8,47 @@ namespace MyConsoleApp
 {
     public class SnakeAndLadder
     {
-        private int[] board;
-        private int WinPos;
-        private int[] playerPos;
+        private int[] _Iobjboard;
+     
+        private int[] _IobjplayerPos;
         
 
         public SnakeAndLadder()
         {
-            board = new int[100];
-            playerPos = new int[2];
-            WinPos = 100;
-            CreateSnakeAndLaddeBoard();
+            _Iobjboard = new int[100];
+            _IobjplayerPos = new int[2];
+
+            CreateSnakeAndLaddeBoardStructure();
         }
-        private void  CreateSnakeAndLaddeBoard()
+        private void  CreateSnakeAndLaddeBoardStructure()
         {
             // create Board;
-            for(int lnCnt = 0; lnCnt < board.Length; lnCnt++)
+            for(int lnCnt = 0; lnCnt < _Iobjboard.Length; lnCnt++)
             {
-                board[lnCnt] = lnCnt + 1;
+                _Iobjboard[lnCnt] = lnCnt + 1;
             }
 
 
             // Snake Create
 
-            board[17] = 7;
-            board[62] = 19;
-            board[64] = 60 ;
-            board[54] = 34;
-            board[87] = 36;
-            board[93] = 73;
-            board[95] = 75;
-            board[96] = 79;
+            _Iobjboard[62] = 19;
+            _Iobjboard[64] = 60 ;
+            _Iobjboard[54] = 34;
+            _Iobjboard[87] = 36;
+            _Iobjboard[93] = 73;
+            _Iobjboard[95] = 75;
+            _Iobjboard[96] = 79;
 
 
             //ladder
-            board[4] = 14;
-            board[9] = 31;
-            board[2] = 38;
-            board[21] = 42;
-            board[51] = 67;
-            board[28] = 84;
-            board[80] = 99;
-            board[72] = 91;
+            _Iobjboard[4] = 14;
+            _Iobjboard[9] = 31;
+            _Iobjboard[2] = 38;
+            _Iobjboard[21] = 42;
+            _Iobjboard[51] = 67;
+            _Iobjboard[28] = 84;
+            _Iobjboard[80] = 99;
+            _Iobjboard[72] = 91;
 
 
         }
@@ -59,11 +58,11 @@ namespace MyConsoleApp
             while (true)
             {
                 Console.WriteLine("Current Player :- " + (currentPlayer + 1)+ "   And Previous Pos :-  "+
-                    playerPos[currentPlayer]);
+                    _IobjplayerPos[currentPlayer]);
                 PlayTurn(currentPlayer);
                
                 Console.ReadKey();
-                if (playerPos[currentPlayer] == WinPos)
+                if (_IobjplayerPos[currentPlayer] == 100)
                 {
                     Console.WriteLine($"Player {currentPlayer + 1} wins!");
                     break;
@@ -76,19 +75,19 @@ namespace MyConsoleApp
         private void PlayTurn(int player)
         {
            
-            Random random = new Random();
-            int diceValue = random.Next(1, 7);
+            Random lnrandom = new Random();
+            int diceValue = lnrandom.Next(1, 7);
 
-            
-            playerPos[player] += diceValue;
+
+            _IobjplayerPos[player] += diceValue;
 
  
-            if (playerPos[player] < board.Length)
+            if (_IobjplayerPos[player] < _Iobjboard.Length)
             {
-                playerPos[player] = board[playerPos[player] - 1];
+                _IobjplayerPos[player] = _Iobjboard[_IobjplayerPos[player] - 1];
             }
 
-            Console.WriteLine($"Player {player + 1} rolled {diceValue}, moved to position {playerPos[player]}");
+            Console.WriteLine($"Player {player + 1} rolled {diceValue}, moved to position {_IobjplayerPos[player]}");
         }
     }
 
